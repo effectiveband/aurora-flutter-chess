bf001202abc2a819f98ae79cb0a6de343a668bb5
neekeetuh <nikitakhimchenko@gmail.com>
2025-03-26 19:25:41 -0700

    fix: добавлена адаптивность для отображения элементов на главном экране

--- frontend/lib/views/menu_view/menu_page_view.dart
+++ frontend/lib/views/menu_view/menu_page_view.dart
@@ -14,7 +14,6 @@ class MyMenuView extends StatefulWidget {
 }
 
 class _MyMenuViewState extends State<MyMenuView> {
-
   @override
   initState() {
     super.initState();
@@ -26,88 +25,91 @@ class _MyMenuViewState extends State<MyMenuView> {
     final scheme = Theme.of(context).colorScheme;
     final width = MediaQuery.of(context).size.width;
     final height = MediaQuery.of(context).size.height;
-    final aspectRatio = height / width;
     return Scaffold(
       backgroundColor: scheme.background,
-      body: Consumer<GameModel>(
-        builder: (context, gameModel, child) {
-          return SafeArea(
-            child: ConstrainedBox(
-              constraints: BoxConstraints(minWidth: width, minHeight: height),
-              child: IntrinsicHeight(
-                child: Container(
-                  margin: const EdgeInsets.symmetric(horizontal: 25),
-                  child: Stack(
-                    children: [
-                      Column(
-                        children: [
-                          const SizedBox(
-                            height: 24,
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              CustomSwitch(provider),
-                              ButtonToGuide(
-                                backGroundColor: scheme.secondaryContainer,
-                                height: 40,
-                                width: 40,
-                                onTap: () {
-                                  context.push(RouteLocations.guidebookScreen);
-                                },
-                              ),
-                            ],
-                          ),
-                          const SizedBox(
-                            height: 40,
-                          ),
-                          Text(
+      body: Consumer<GameModel>(builder: (context, gameModel, child) {
+        return SafeArea(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(minWidth: width, minHeight: height),
+            child: IntrinsicHeight(
+              child: Container(
+                margin: const EdgeInsets.symmetric(horizontal: 25),
+                child: Stack(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(
+                          height: 24,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            CustomSwitch(provider),
+                            ButtonToGuide(
+                              backGroundColor: scheme.secondaryContainer,
+                              height: 40,
+                              width: 40,
+                              onTap: () {
+                                context.push(RouteLocations.guidebookScreen);
+                              },
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 40,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 15),
+                          child: Text(
                             MenuPageStringConst.slogan,
                             style: TextStyle(
-                              fontSize: 36,
-                              fontFamily: "Roboto",
-                              fontStyle: FontStyle.normal,
-                              fontWeight: FontWeight.w600,
-                              color: scheme.primary
-                            ),
-                          ),
-                          Container(
-                            alignment: Alignment.center,
-                            child: SvgPicture.asset(
-                              "${MenuPageStringConst.pathToIcon}pieces.svg",
-                              width: aspectRatio > 2
-                                  ? width * 0.9
-                                  : (height - width) * 0.8,
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 48,
-                          ),
-                        ],
-                      ),
-                      Align(
-                        alignment: Alignment.bottomCenter,
-                        child: Padding(
-                          padding: const EdgeInsets.only(bottom: 20),
-                          child: NextPageButton(
-                            text: MenuPageStringConst.localButton,
-                            textColor: ColorsConst.primaryColor0,
-                            buttonColor: scheme.secondaryContainer,
-                            isClickable: true,
-                            onTap: () {
-                              context.go(RouteLocations.settingsScreen, extra: gameModel);
-                            },
+                                fontSize: 36,
+                                fontFamily: "Roboto",
+                                fontStyle: FontStyle.normal,
+                                fontWeight: FontWeight.w600,
+                                color: scheme.primary),
                           ),
                         ),
+                        Expanded(
+                          child: Padding(
+                            padding: EdgeInsets.only(
+                                top: width * 0.2,
+                                left: width * 0.2,
+                                bottom: height * 0.12),
+                            child: SizedBox(
+                              width: double.infinity,
+                              child: SvgPicture.asset(
+                                "${MenuPageStringConst.pathToIcon}pieces.svg",
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Padding(
+                        padding: const EdgeInsets.only(bottom: 20),
+                        child: NextPageButton(
+                          text: MenuPageStringConst.localButton,
+                          textColor: ColorsConst.primaryColor0,
+                          buttonColor: scheme.secondaryContainer,
+                          isClickable: true,
+                          onTap: () {
+                            context.go(RouteLocations.settingsScreen,
+                                extra: gameModel);
+                          },
+                        ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
             ),
-          );
-        }
-      ),
+          ),
+        );
+      }),
     );
   }
 }
