98e70daddec6646c6c23c7bdd1fe54a1341d04bb
EkaterinaMarkina-1 <katya.05.2004.km@gmail.com>
2025-04-03 18:55:00 +0600

    refactor : убран закомментированный код

--- frontend/lib/views/setting_view/constants/game_setting_const.dart
+++ frontend/lib/views/setting_view/constants/game_setting_const.dart
@@ -1,5 +1,3 @@
-// import "../game_settings_view.dart";
-
 class GameSettingConsts {
   static String appBarLabel = "Параметры";
   static String gameModeText = "Режим игры";
@@ -65,18 +63,6 @@ class GameSettingConsts {
     45,
     60
   ];
-
-  // static Map<LevelOfDifficulty, int> difficultyLevels = {
-  //   LevelOfDifficulty.easy: 1,
-  //   LevelOfDifficulty.medium: 3,
-  //   LevelOfDifficulty.hard: 5,
-  // };
-
-  // static Map<LevelOfDifficulty, String> personalLevelOfDifficultyText = {
-  //   LevelOfDifficulty.easy: "Лёгкий",
-  //   LevelOfDifficulty.medium: "Средний",
-  //   LevelOfDifficulty.hard: "Сложный",
-  // };
 
   static String dbCreateScript = """CREATE TABLE Settings (
   id INTEGER PRIMARY KEY, 
