88c22e0fa417f9b0d214a1f278f4f5d8f0127607
neekeetuh <nikitakhimchenko@gmail.com>
2025-04-05 11:37:25 +0600

    fix: изменения расчета бокового отступа для виджета доски на экране игры в зависимости от соотношения сторон экрана

--- frontend/lib/views/game_view/game_page_view.dart
+++ frontend/lib/views/game_view/game_page_view.dart
@@ -1,3 +1,5 @@
+import "dart:math";
+
 import "package:flutter/material.dart";
 import "package:flutter_svg/flutter_svg.dart";
 import "package:go_router/go_router.dart";
@@ -60,46 +62,50 @@ class _GameViewState extends State<GameView> {
                                   child: BackArrowButton(gameModel)),
                             ),
                             SizedBox(height: deviceHeight * 0.02),
-                            Padding(
-                              padding: deviceWidth / deviceHeight < 0.65
-                                  ? EdgeInsets.zero
-                                  : EdgeInsets.symmetric(
-                                      horizontal: deviceWidth * 0.1),
-                              child: Column(
-                                children: [
-                                  PlayerAndTimerWidget(
-                                    gameModel: gameModel,
-                                    currentPlayer: gameModel.playerSide,
-                                  ),
-                                  Padding(
-                                    padding: EdgeInsets.symmetric(
-                                        vertical: deviceHeight * 0.015),
-                                    child: FittedBox(
-                                      child: Stack(
-                                        children: [
-                                          Align(
-                                            alignment: Alignment.topCenter,
-                                            child: SvgPicture.asset(
-                                              "assets/images/board.svg",
-                                              width: deviceWidth,
-                                              height: deviceWidth *
-                                                  LogicConsts.boardRatio,
-                                            ),
-                                          ),
-                                          Align(
+                            FittedBox(
+                              child: Padding(
+                                padding: deviceWidth / deviceHeight < 0.65
+                                    ? EdgeInsets.zero
+                                    : EdgeInsets.symmetric(
+                                        horizontal: deviceWidth *
+                                            0.3 *
+                                            pow(deviceWidth / deviceHeight, 5)),
+                                child: Column(
+                                  children: [
+                                    PlayerAndTimerWidget(
+                                      gameModel: gameModel,
+                                      currentPlayer: gameModel.playerSide,
+                                    ),
+                                    Padding(
+                                      padding: EdgeInsets.symmetric(
+                                          vertical: deviceHeight * 0.015),
+                                      child: FittedBox(
+                                        child: Stack(
+                                          children: [
+                                            Align(
                                               alignment: Alignment.topCenter,
-                                              child:
-                                                  ChessBoardWidget(gameModel)),
-                                        ],
+                                              child: SvgPicture.asset(
+                                                "assets/images/board.svg",
+                                                width: deviceWidth,
+                                                height: deviceWidth *
+                                                    LogicConsts.boardRatio,
+                                              ),
+                                            ),
+                                            Align(
+                                                alignment: Alignment.topCenter,
+                                                child: ChessBoardWidget(
+                                                    gameModel)),
+                                          ],
+                                        ),
                                       ),
                                     ),
-                                  ),
-                                  PlayerAndTimerWidget(
-                                    gameModel: gameModel,
-                                    currentPlayer:
-                                        oppositePlayer(gameModel.playerSide),
-                                  ),
-                                ],
+                                    PlayerAndTimerWidget(
+                                      gameModel: gameModel,
+                                      currentPlayer:
+                                          oppositePlayer(gameModel.playerSide),
+                                    ),
+                                  ],
+                                ),
                               ),
                             ),
                             const Spacer(),
